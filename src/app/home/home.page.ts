a06c2318d94eb74fb1e4270fd14aa71d29247bbc
Stephane Thiebaut <stephanethiebautjob@gmail.com>
2023-04-30 19:51:25 +0200

    Initial Commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
-
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Storage } from '@ionic/storage';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  loginForm!: FormGroup;
+  constructor(private fb: FormBuilder) {}
 
-  constructor() {}
+  ngOnInit(): void{
+    this.loginForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.minLength(6)])
+    });
+  }
+
+  onSubmit() {
+    // Do something with the form data
+    console.log(this.loginForm.value);
+  }
 
 }
